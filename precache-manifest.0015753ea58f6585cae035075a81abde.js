811a752804319049965fec3123897bb5af098266
christine <bumpouce@gmail.com>
2020-07-21 13:25:06 -0700

    Updates

---
+++ precache-manifest.0015753ea58f6585cae035075a81abde.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34eee4bd9a81d322cb964cd9524837ff",
+    "url": "/speedRead-FrontEnd/index.html"
+  },
+  {
+    "revision": "50e730bfd91a6bd197f3",
+    "url": "/speedRead-FrontEnd/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "822b10656535ded915f3",
+    "url": "/speedRead-FrontEnd/static/css/main.915d0bee.chunk.css"
+  },
+  {
+    "revision": "50e730bfd91a6bd197f3",
+    "url": "/speedRead-FrontEnd/static/js/2.c0df8ed8.chunk.js"
+  },
+  {
+    "revision": "0f2750f66c8209bbfef3371f71d52359",
+    "url": "/speedRead-FrontEnd/static/js/2.c0df8ed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "822b10656535ded915f3",
+    "url": "/speedRead-FrontEnd/static/js/main.aa00a361.chunk.js"
+  },
+  {
+    "revision": "647c1f6d738ecf9f3c82",
+    "url": "/speedRead-FrontEnd/static/js/runtime-main.76249f31.js"
+  },
+  {
+    "revision": "cce6e706dddf31753a783f17d5bd4dcc",
+    "url": "/speedRead-FrontEnd/static/media/crisp-paper-ruffles.cce6e706.png"
+  },
+  {
+    "revision": "e8729278d5d75f7c26c21a7b0e1e6f88",
+    "url": "/speedRead-FrontEnd/static/media/lined-paper.e8729278.png"
+  },
+  {
+    "revision": "9befbfcfd41cca3886d29e15d6bbfa38",
+    "url": "/speedRead-FrontEnd/static/media/otis-redding.9befbfcf.png"
+  }
+]);
